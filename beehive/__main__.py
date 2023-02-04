ba11a506f2b776242d2c111b1d8ed450ade566d1
Aadhithya <aadhithya.s@outlook.com>
2023-02-04 01:49:07 +0100

    feat: set up app

--- beehive/__main__.py
+++ beehive/__main__.py
@@ -1,2 +1,23 @@
+from loguru import logger
+from typer import Argument, Option, Typer
+
+app = Typer(name="beehive")
+
+
+@app.command("train", help="Train a model.")
+def train():
+    pass
+
+
+@app.command("eval", help="Evaluate a trained model.")
+def eval():
+    pass
+
+
+@app.command("infer", help="Run inference using a trained model.")
+def infer():
+    pass
+
+
 if __name__ == "__main__":
-    print("Hello, world!")
+    app()
