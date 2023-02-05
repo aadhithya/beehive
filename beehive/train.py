fff40ac20e7dbce0a0e65b0c3186b625702bc385
Aadhithya <aadhithya.s@outlook.com>
2023-02-05 01:21:22 +0100

    feat(train.py): add function to train model

---
+++ beehive/train.py
@@ -0,0 +1,97 @@
+from typing import List, Optional
+
+import pytorch_lightning as pl
+import torch
+from loguru import logger
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from torch.utils.data import DataLoader
+
+from beehive.dataset import BeehiveDataset, pad_collate_fn
+from beehive.model import CenterNet
+
+
+def train_model(
+    # * dataset params
+    data_dir: str,
+    split_file: str,
+    batch_size: Optional[int] = 8,
+    # * model params
+    n_classes: Optional[int] = 1,
+    n_conv: Optional[int] = 2,
+    head_convs: Optional[int] = 2,
+    upsample_channels: Optional[List[int]] = [256, 128, 64],
+    bilinear: Optional[bool] = True,
+    backbone: Optional[str] = "r18",
+    pretrained: Optional[bool] = True,
+    # * training params
+    lr: Optional[float] = 5e-4,
+    lamda_kp: Optional[float] = 1.0,
+    lamda_offset: Optional[float] = 1.0,
+    alpha_kp: Optional[int] = 2,
+    beta_kp: Optional[int] = 2,
+    epochs: Optional[int] = 10,
+):
+    pl.seed_everything(42)
+    logger.info("Seed set...")
+    logger.info("Creating model...")
+    model = CenterNet(
+        n_classes=n_classes,
+        n_conv=n_conv,
+        head_convs=head_convs,
+        upsample_channels=upsample_channels,
+        bilinear=bilinear,
+        backbone=backbone,
+        pretrained=pretrained,
+        lr=lr,
+        lamda_kp=lamda_kp,
+        lamda_offset=lamda_offset,
+        alpha_kp=alpha_kp,
+        beta_kp=beta_kp,
+    )
+
+    logger.info("Creating training and validation datasets...")
+    train_dataset = BeehiveDataset(
+        data_dir=data_dir, split_json=split_file, split="train"
+    )
+
+    val_dataset = BeehiveDataset(
+        data_dir=data_dir, split_json=split_file, split="val"
+    )
+
+    logger.info("Creating dataloaders for the datasets...")
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        collate_fn=pad_collate_fn,
+        drop_last=False,
+    )
+
+    val_dataloader = DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        collate_fn=pad_collate_fn,
+        drop_last=False,
+    )
+
+    # early_stop_callback = EarlyStopping(
+    #     monitor="val-loss",
+    #     min_delta=0.00,
+    #     patience=5,
+    #     verbose=False,
+    #     mode="max",
+    # )
+
+    trainer = pl.Trainer(
+        # callbacks=[early_stop_callback],
+        max_steps=epochs,
+        log_every_n_steps=5,
+        accelerator="gpu" if torch.cuda.is_available() else "cpu",
+    )
+
+    logger.info("Starting model training...")
+    trainer.fit(
+        model=model,
+        train_dataloaders=train_dataloader,
+        val_dataloaders=val_dataloader,
+    )
+    logger.info("Starting model training... Done.")
